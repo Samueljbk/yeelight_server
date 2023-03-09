778c1c27b730f2458782ba5c5013e872d104dbb1
Jerome Swannack <jeromeswannack@gmail.com>
2023-03-09 21:02:14 +1300

    Added sample yeelight automation script to main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+import yeelight
+
+
+_BULB_NAME = "sams-yeelight"
+
+
+def main():
+    # get all bulbs on the network
+    bulbs = yeelight.discover_bulbs()
+
+    # find the bulb you want to control
+    bulb = next(bulb for bulb in bulbs if bulb["capabilities"]["name"] == _BULB_NAME)
+
+    # create a Yeelight object
+    yeelight_bulb = yeelight.Bulb(bulb["ip"])
+
+    # turn on the bulb
+    yeelight_bulb.turn_on()
+
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    main()
