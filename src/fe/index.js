a0cd84c7ae3c41b0051a782baeef1058a38e33ce
Jerome Swannack <jeromeswannack@gmail.com>
2023-03-24 08:17:06 +1300

    Added js for new buttons

--- src/fe/index.js
+++ src/fe/index.js
@@ -55,3 +55,53 @@ function turnOff() {
 
   })
 }
+
+// setColour light function
+function setColour() {
+  // get the rgb colour values from the 3 input fields
+  var red = document.getElementById('red').value;
+  var green = document.getElementById('green').value;
+  var blue = document.getElementById('blue').value;
+
+  var colour = [red, green, blue];
+
+  // post the /api/vi/set_colour endpoint
+  fetch('/api/v1/set_colour', {
+    method: 'POST'
+    body: JSON.stringify(colour)
+  }).then(function(response) {
+    // get the response span
+    var responseSpan = document.getElementById(
+      'set-colour-response'
+    );
+    // set the response span text to the response text content
+    // don't forget to get the result of the promise
+    response.text().then(function(text) {
+      responseSpan.textContent = text;
+    })
+
+  })
+}
+
+// setBrightness light function
+function setBrightness() {
+  // get the brightness value from the input field
+  var brightness = document.getElementById('brightness').value;
+
+  // post the /api/vi/set_brightness endpoint
+  fetch('/api/v1/set_brightness', {
+    method: 'POST'
+    body: JSON.stringify(brightness)
+  }).then(function(response) {
+    // get the response span
+    var responseSpan = document.getElementById(
+      'set-brightness-response'
+    );
+    // set the response span text to the response text content
+    // don't forget to get the result of the promise
+    response.text().then(function(text) {
+      responseSpan.textContent = text;
+    })
+
+  })
+}
