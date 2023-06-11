33a13c59a51926e53a4ce99adbdf39c27f5decf3
Samueljbk <samjbkyle@gmail.com>
2023-06-12 08:10:38 +1200

    fixing ruff linter conditions

--- src/scripts/github_og.py
+++ src/scripts/github_og.py
@@ -1,7 +1,6 @@
 from github import Github
 import os
 import datetime
-import time
 
 token = os.environ["PERSONAL_ACCESS_TOKEN"]
 
@@ -19,6 +18,7 @@ print(latest_event)
 
 # TODO GOAL: Set light colour to be one colour before commiting and one colour after
 
-# TODO: Write has_commited function, colour functions for before and after, time of day function (is_past_seven or is_past_due_date etc)
+# TODO: Write has_commited function, colour functions for before and after,
+# time of day function (is_past_seven or is_past_due_date etc)
 # Make a while True loop with a sleep that calls the correct colour function
 # Look at discord controller for inso on how to do bulb functions
