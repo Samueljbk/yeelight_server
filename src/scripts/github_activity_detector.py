b41f65fb18354e90e0a3ce0911033825f070bbce
Samueljbk <samjbkyle@gmail.com>
2023-06-12 08:08:49 +1200

    Fixing while loop bug to check constantly rather than once at midnight

--- src/scripts/github_activity_detector.py
+++ src/scripts/github_activity_detector.py
@@ -7,17 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-yeelight_ip = "192.168.86.122"
+yeelight_ip = os.environ["BULB_IP"]
 bulb = Bulb(yeelight_ip)
 token = os.environ["PERSONAL_ACCESS_TOKEN"]
 
 g = Github(token)
 
 user = g.get_user("Samueljbk")
-all_events = list(user.get_events())
-push_events = [e for e in all_events if e.type == "PushEvent"]
-latest_event = max(push_events, key=lambda e: e.created_at)
-latest_event_time = latest_event.created_at
 
 
 def set_light_colour(colour):
@@ -28,6 +24,11 @@ def set_light_colour(colour):
 
 
 def check_commit_status():
+    all_events = list(user.get_events())
+    push_events = [e for e in all_events if e.type == "PushEvent"]
+    latest_event = max(push_events, key=lambda e: e.created_at)
+    latest_event_time = latest_event.created_at
+
     if latest_event_time.date() == datetime.date.today():
         print("Pushed Today")
         set_light_colour("green")
@@ -41,12 +42,15 @@ check_commit_status()
 while True:
     current_time = datetime.datetime.now()
     if current_time.hour == 0 and current_time.minute == 0:
-        check_commit_status()
+        set_light_colour("red")
         time.sleep(60)
-    time.sleep(10)
+    else:
+        check_commit_status()
+        time.sleep(10)
 
 # TODO GOAL: Set light colour to be one colour before commiting and one colour after
 
-# TODO: Write has_commited function, colour functions for before and after, time of day function (is_past_seven or is_past_due_date etc)
+# TODO: Write has_commited function, colour functions for before and after, time of day function
+# (is_past_seven or is_past_due_date etc)
 # Make a while True loop with a sleep that calls the correct colour function
 # Look at discord controller for inso on how to do bulb functions
